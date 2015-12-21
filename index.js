f7de3c651101495b65c624a8c07295e04edf3324
eliot-akira <me@eliotakira.com>
2015-12-21 02:52:32 +0100

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+var expandChildren = function(children) {
+  var buff, child, i, len;
+  console.log('exp', children);
+  if (typeof children === 'string') {
+    return children;
+  }
+  buff = '';
+  for (i = 0, len = children.length; i < len; i++) {
+    child = children[i];
+    if (child.length === 1) {
+      buff += child.pop;
+    } else {
+      buff += expandChildren(child);
+    }
+  }
+  return buff;
+};
+var jsxhtml = function() {
+  var attrs, buf, children, key,
+    slice = [].slice, tag, value;
+  tag = arguments[0];
+  attrs = arguments[1];
+  children = 3 <= arguments.length ? slice.call(arguments, 2) : [];
+  console.log('crel', tag, attrs, children);
+  buf = "<" + tag;
+  if ((attrs != null) && typeof attrs === 'object') {
+    for (key in attrs) {
+      value = attrs[key];
+      buf += " " + key + "=\"" + value + "\"";
+    }
+  }
+  if ((children == null) || children.length === 0) {
+    buf += ' />';
+    return buf;
+  } else {
+    buf += '>';
+    buf += expandChildren(children);
+    buf += "</" + tag + ">";
+  }
+  return buf;
+};
+global.jsxhtml = module.exports = jsxhtml;
